f00b88d30738404e5cbe2247a6573b6032ea0a2a
William Naughton <66079303+Archadox@users.noreply.github.com>
2021-03-26 20:14:04 -0400

    Update main.rs
    Le many comments

--- is_burger/src/main.rs
+++ is_burger/src/main.rs
@@ -1,17 +1,17 @@
-use std::io;
+use std::io; //le import
 
-fn main() {
-	println!("Type \"Burger\" or \"burger\"");
-	let mut input = String::new();
-	io::stdin()
-	    .read_line(&mut input)
-	    .expect("idk lol");
+fn main() { //the main function of the code
+	println!("Type \"Burger\" or \"burger\""); //tells the user what to type
+	let mut input = String::new(); //creates the varable to save user input to
+	io::stdin() 
+	    .read_line(&mut input) //read the input of the user and saves it to the "input" variable
+	    .expect("idk lol"); //something to prevent error lmao
 
-//    println!("You typed {}", input);
-    if input == "Burger\n" || input == "burger\n"{
-        println!("Your word is burger! Congratulations!");
+//    println!("You typed {}", input); //commented out rn, but used for testing mainly
+    if input == "Burger\n" || input == "burger\n"{ //comparing the user's input to the word "burger", capitalized and not
+        println!("Your word is burger! Congratulations!"); //response when the user correctly types the word "burger"
     } 
     else {
-    	println!("Your word is not burger, stupid.")
+    	println!("Your word is not burger, stupid.") //when the user is idiotic and types something other than "burger"
     }
 }
